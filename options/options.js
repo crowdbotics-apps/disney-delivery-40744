67efe58dfae678b9cbfb5fe7ca84a401a6a40641
Crowdbotics <team@crowdbotics.com>
2023-05-16 20:09:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "disney-delivery-40744",
+  url: "https://disney-delivery-40744.botics.co",
+  api: "https://disney-delivery-40744.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
